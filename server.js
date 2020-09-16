d70e16055b098800fb95f82728abeb7d56c7cc4a
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-16 04:41:45 -0400

    created and pseudocoded server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// Declared dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+// Declared PORT at 3000
+const PORT = 3000;
+
+// Declared app to express() method
+const app = express();
+
+// Asked app to use logger("dev"), compression(), urlencoded to true, json() and static();
+
+
+// Created mongoose db connection
+
+
+// Established route
+
+
+// Created app listener to run on port 3000
