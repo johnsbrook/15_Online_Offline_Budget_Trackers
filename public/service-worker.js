8cb318b848bdbdea1ee377b799e8b618c8759ff8
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-16 19:04:25 -0400

    Create self addEventListener() to install with a function to wait until files have been pre-cached inside public/service-worker.js file

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,18 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // Create self addEventListener() to install with a function to wait until files have been pre-cached by opening CACHE_NAME as an argument and adding all FILES_TO_CACHE as a return
+self.addEventListener("install", function(event) {
+    
+    // Event to pre-cache files
+    event.waitUntil(
+        caches.open(CACHE_NAME) // If code doesn't work, join ".then..." line here
+        .then(cache => {
+            console.log(`Files pre-cached successfully.`);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+})
 
 
 // Create self addEventListener() to activate with a function and mapped
