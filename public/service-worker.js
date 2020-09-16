1c2f9f297b85ff4b310d08b5e96792c724d164ad
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-16 18:58:51 -0400

    added const CACHE_NAME and DATA_CACHE_NAME inside public/service-worker.js file'

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,10 @@
 const FILES_TO_CACHE = ["/", "/index.html", "index.js", "styles.css"];
 
 // Declare CACHE_NAME as static-cache-v2
-
+const CACHE_NAME = "static-cache-v2";
 
 // Declare DATA_CACHE_NAME as data-cache-v1
-
+const DATA_CACHE_NAME = "data-cache-v1";
 
 // Create self addEventListener() to install with a function to wait until files have been pre-cached by opening CACHE_NAME as an argument and adding all FILES_TO_CACHE as a return
 
