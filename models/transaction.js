efd4913dbb60aef7a468fc9cd2d651fad489f9b0
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-16 05:03:28 -0400

    created and pseudocoded models/transaction.js

---
+++ models/transaction.js
@@ -0,0 +1,13 @@
+// Declare mongoose to require "mongoose"
+
+
+// Declare Schema to mongoose.schema
+
+
+// Declare transactionSchema and construct new Schema for name (trimmed and required), value (required), and date (defaulted at Date.now)
+
+
+// Declare Transaction module as a mongoose model named "Transaction" using transactionSchema
+
+
+// Export Transaction module
