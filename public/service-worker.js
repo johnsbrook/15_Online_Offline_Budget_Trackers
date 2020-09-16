43346ed2415271b8976a122d3aadb3ce9499f630
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-16 18:56:47 -0400

    declared array of FILES_TO_CACHE_ inside an array of strings

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-// Declare FILES_TO_CACHE inside an array of strings
-
+// Declare an array of FILES_TO_CACHE inside an array of strings
+const FILES_TO_CACHE = ["/", "/index.html", "index.js", "styles.css"];
 
 // Declare CACHE_NAME as static-cache-v2
 
@@ -8,3 +8,11 @@
 
 
 // Create self addEventListener() to install with a function to wait until files have been pre-cached by opening CACHE_NAME as an argument and adding all FILES_TO_CACHE as a return
+
+
+// Create self addEventListener() to activate with a function and mapped
+
+
+// Create self addEventListener() to fetch api to clone and store response in cache
+
+
