f30fdfba2fce8ce2bdd0f8ffa72996cdb5741d5c
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-16 05:07:27 -0400

    created transaction.js model for mongoose db

--- models/transaction.js
+++ models/transaction.js
@@ -1,13 +1,34 @@
 // Declare mongoose to require "mongoose"
-
+const mongoose = require("mongoose");
 
 // Declare Schema to mongoose.schema
-
+const Schema = mongoose.Schema;
 
 // Declare transactionSchema and construct new Schema for name (trimmed and required), value (required), and date (defaulted at Date.now)
-
+const transactionSchema = new Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter transaction name."
+        }
+    },
+    {
+        value: {
+            type: Number,
+            required: "Enter transaction value."
+        }
+    },
+    {
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
 
 // Declare Transaction module as a mongoose model named "Transaction" using transactionSchema
-
+const Transaction = mongoose.model("Transaction", transactionSchema)
 
 // Export Transaction module
+module.exports = Transaction;
