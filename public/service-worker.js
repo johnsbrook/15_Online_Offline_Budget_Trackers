7020234d852019ca04a8692bcdabdbe62670c835
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-16 14:38:06 -0400

    began pseudocoding public/service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+// Declare FILES_TO_CACHE inside an array of strings
+
+
+// Declare CACHE_NAME as static-cache-v2
+
+
+// Declare DATA_CACHE_NAME as data-cache-v1
+
+
+// Create self addEventListener() to install with a function to wait until files have been pre-cached by opening CACHE_NAME as an argument and adding all FILES_TO_CACHE as a return
